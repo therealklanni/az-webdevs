8e3a3f0af61062fdf49dae432455a0cac1a21340
Kevin Lanni <therealklanni@gmail.com>
2015-12-25 21:42:05 -0700

    integrate views with db
    closes #12

--- routes/apply/index.js
+++ routes/apply/index.js
@@ -7,6 +7,7 @@ import validate from '../../lib/validate'
 import rateLimit from '../../lib/rate-limit'
 import slackApi from '../../lib/slack'
 import { exitWithError, getStrings } from '../../lib/helpers'
+import User from '../../lib/db/models/user'
 
 const debug = bug('SIR:apply')
 const router = express.Router()
@@ -16,62 +17,76 @@ const botName = process.env.SLACK_BOT_NAME || 'SIR';
 const slackUrl = process.env.SLACK_WEBHOOK_URL
 const slack = slackUrl ? slackApi(slackUrl) : exitWithError('Please set SLACK_WEBHOOK_URL environment variable.')
 
-const getUser = _.partialRight(dotty.get, 'session.passport.user')
+const getId = _.partialRight(dotty.get, 'session.passport.user')
 
 router.get('/', validate, (req, res) => {
-  const user = getUser(req)
+  const githubId = getId(req)
   const strings = getStrings()
 
-  strings.apply.form.fullName.value = user.displayName
-  strings.apply.form.email.value = user.emails[0].value
+  User.findOne({ githubId }, (err, user) => {
+    if (err || !user) {
+      debug(`Could not find user ${githubId}`)
+      return res.redirect('/')
+    }
 
-  res.render('apply', _.assign({}, strings.apply, user))
+    strings.apply.form.fullName.value = user.displayName
+    strings.apply.form.email.value = user.email
+
+    res.render('apply', _.assign({}, strings.apply, user))
+  })
 })
 
 router.post('/', validate, rateLimit(), (req, res) => {
-  const user = getUser(req)
+  const githubId = getId(req)
 
-  debug('Received application from "%s <%s>"', user.displayName, user.emails[0].value)
+  User.findOne({ githubId }, (err, user) => {
+    if (err || !user) {
+      debug(`Could not find user ${githubId}`)
+      return res.redirect('/')
+    }
 
-  res.redirect('/thanks')
+    debug('Received application from "%s <%s>"', user.displayName, user.email)
 
-  slack({
-    channel: channel,
-    username: botName,
-    icon_url: req.originUri + 'images/bot.png',
-    attachments: [{
-      fallback: user.displayName + ' wants to join Slack',
-      author_name: user.displayName,
-      author_link: user.url,
-      author_icon: dotty.get(user, 'image.url'),
-      color: '#28f428',
-      pretext: 'New invite request:',
-      text: req.body.comments ? `${req.body.comments}` : undefined,
-      fields: _.map(
-        // omit comments because it's used for "text" above
-        _.pairs(_.omit(req.body, 'comments')),
-        // transform the field data
-        _.flow(
-          // necessary for unknown reasons
-          x => x,
-          // convert field names to title case
-          _.partialRight(_.map, (str, i) => {
-            return i ? str : changeCase.title(str)
-          }),
-          // convert array values (e.g. checbox fields) into a string
-          x => {
-            if (_.isArray(x[1])) {
-              x[1] = x[1].join(', ')
-            }
-            return x
-          },
-          // combine title and value into an object
-          _.partial(_.zipObject, ['title', 'value']),
-          // add "short" property to object
-          _.partialRight(_.assign, { short: true })
+    res.redirect('/thanks')
+
+    slack({
+      channel: channel,
+      username: botName,
+      icon_url: req.originUri + 'images/bot.png',
+      attachments: [{
+        fallback: user.displayName + ' wants to join Slack',
+        author_name: user.displayName,
+        author_link: user.url,
+        author_icon: dotty.get(user, 'image.url'),
+        color: '#28f428',
+        pretext: 'New invite request:',
+        text: req.body.comments ? `${req.body.comments}` : undefined,
+        fields: _.map(
+          // omit comments because it's used for "text" above
+          _.pairs(_.omit(req.body, 'comments')),
+          // transform the field data
+          _.flow(
+            // necessary for unknown reasons
+            x => x,
+            // convert field names to title case
+            _.partialRight(_.map, (str, i) => {
+              return i ? str : changeCase.title(str)
+            }),
+            // convert array values (e.g. checbox fields) into a string
+            x => {
+              if (_.isArray(x[1])) {
+                x[1] = x[1].join(', ')
+              }
+              return x
+            },
+            // combine title and value into an object
+            _.partial(_.zipObject, ['title', 'value']),
+            // add "short" property to object
+            _.partialRight(_.assign, { short: true })
+          )
         )
-      )
-    }]
+      }]
+    })
   })
 })
 
--- routes/thanks/index.js
+++ routes/thanks/index.js
@@ -2,11 +2,25 @@ import _ from 'lodash'
 import dotty from 'dotty'
 import validate from '../../lib/validate'
 import { getStrings } from '../../lib/helpers'
+import User from '../../lib/db/models/user'
 import express from 'express'
+import bug from 'debug'
+
+const debug = bug('SIR:thanks')
 const router = express.Router()
 
 router.get('/', validate, (req, res) => {
-  res.render('thanks', _.assign({}, getStrings().main, dotty.get(req, 'session.passport.user')))
+  const githubId = dotty.get(req, 'session.passport.user')
+
+  User.findOne({ githubId }, (err, user) => {
+    if (err || !user) {
+      debug(`Could not find user ${githubId}`)
+      return res.redirect('/')
+    }
+
+    res.render('thanks', _.assign({}, getStrings().main, user._doc))
+  })
+
 })
 
 export default router
