50947c4f96afd5676ed00355a99d5d67dd8ef013
Kevin Lanni <therealklanni@gmail.com>
2015-12-25 18:34:08 -0700

    fix missing imports and bad session reference
    in thanks route

--- routes/thanks/index.js
+++ routes/thanks/index.js
@@ -1,9 +1,12 @@
+import _ from 'lodash'
+import dotty from 'dotty'
 import validate from '../../lib/validate'
+import { getStrings } from '../../lib/helpers'
 import express from 'express'
 const router = express.Router()
 
 router.get('/', validate, (req, res) => {
-  res.render('thanks', _.assign({}, getStrings().main, dotty.get(req, 'session.user')))
+  res.render('thanks', _.assign({}, getStrings().main, dotty.get(req, 'session.passport.user')))
 })
 
 export default router
