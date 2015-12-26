ff222adc2db200f6fba6cb87e9e17cd06caee2d2
Kevin Lanni <therealklanni@gmail.com>
2015-12-25 18:34:30 -0700

    remove file handling from apply route

--- routes/apply/index.js
+++ routes/apply/index.js
@@ -10,6 +10,9 @@ import { exitWithError, getStrings } from '../../lib/helpers'
 
 const debug = bug('SIR:apply')
 const router = express.Router()
+
+const channel = process.env.SLACK_CHANNEL;
+const botName = process.env.SLACK_BOT_NAME || 'SIR';
 const slackUrl = process.env.SLACK_WEBHOOK_URL
 const slack = slackUrl ? slackApi(slackUrl) : exitWithError('Please set SLACK_WEBHOOK_URL environment variable.')
 
@@ -27,65 +30,36 @@ router.get('/', validate, (req, res) => {
 
 router.post('/', validate, rateLimit(), (req, res) => {
   const user = getUser(req)
-  const files = req.files
-  const renameJobs = []
 
   debug('Received application from "%s <%s>"', user.displayName, user.emails[0].value)
 
-  for (const field in files) {
-    const fileObj = files[field]
-    const tmpPath = fileObj.path
-    const filename = field + '-' + fileObj.name
-    const dest = __dirname + '/public/images/' + filename
+  res.redirect('/thanks')
 
-    _.assign(fileObj, {
-      dest: dest,
-      uri: req.originUri + '/images/' + filename
-    })
-
-    renameJobs.push(async.apply(mv, tmpPath, dest))
-
-    async.parallel(renameJobs, (err) => {
-      if (err) {
-        error(err)
-        return res.sendStatus(500)
-      }
-
-      res.redirect('/thanks')
-
-      slack({
-      channel: channel,
-      username: botName,
-      icon_url: req.originUri + 'images/bot.png',
-      attachments: [
-        {
-          fallback: user.displayName + ' wants to join Slack',
-          author_name: user.displayName,
-          author_link: user.url,
-          author_icon: dotty.get(user, 'image.url'),
-          color: '#28f428',
-          pretext: 'New invite request:',
-          text: req.body.comments ? '"' + req.body.comments + '"' : undefined,
-          fields: _.map(
-            _.pairs(_.omit(req.body, 'comments')),
-            _.flow(
-              x => x,
-              _.partialRight(_.map, (str, i) => {
-                return i ? str : changeCase.title(str)
-              }),
-              _.partial(_.zipObject, ['title', 'value']),
-              _.partialRight(_.assign, { short: true })
-            )
-          )
-            .concat(_.map(files, (file) => ({
-              title: file.fieldname,
-              value: '<' + file.uri + '|View>',
-              short: true
-            })))
-        }
-      ]
-    })
+  slack({
+    channel: channel,
+    username: botName,
+    icon_url: req.originUri + 'images/bot.png',
+    attachments: [{
+      fallback: user.displayName + ' wants to join Slack',
+      author_name: user.displayName,
+      author_link: user.url,
+      author_icon: dotty.get(user, 'image.url'),
+      color: '#28f428',
+      pretext: 'New invite request:',
+      text: req.body.comments ? '"' + req.body.comments + '"' : undefined,
+      fields: _.map(
+        _.pairs(_.omit(req.body, 'comments')),
+        _.flow(
+          x => x,
+          _.partialRight(_.map, (str, i) => {
+            return i ? str : changeCase.title(str)
+          }),
+          _.partial(_.zipObject, ['title', 'value']),
+          _.partialRight(_.assign, { short: true })
+        )
+      )
+    }]
   })
-}})
+})
 
 export default router
