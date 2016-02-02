50f2d5bb185815e99114429360a2121ab1df4d2a
Kevin Lanni <therealklanni@gmail.com>
2016-02-01 23:09:07 -0700

    include additional github data

--- routes/apply/index.js
+++ routes/apply/index.js
@@ -13,9 +13,10 @@ const debug = bug('SIR:apply')
 const router = express.Router()
 
 const channel = process.env.SLACK_CHANNEL;
-const botName = process.env.SLACK_BOT_NAME || 'SIR';
+const botName = process.env.SLACK_BOT_NAME || 'SIR'
 const slackUrl = process.env.SLACK_WEBHOOK_URL
-const slack = slackUrl ? slackApi(slackUrl) : exitWithError('Please set SLACK_WEBHOOK_URL environment variable.')
+const slack = slackUrl ? slackApi(slackUrl) :
+  exitWithError('Please set SLACK_WEBHOOK_URL environment variable.')
 
 const getId = _.partialRight(dotty.get, 'session.passport.user')
 
@@ -29,7 +30,7 @@ router.get('/', validate, (req, res) => {
       return res.redirect('/')
     }
 
-    strings.apply.form.fullName.value = user.displayName
+    strings.apply.form.fullName.value = user.name
     strings.apply.form.email.value = user.email
 
     res.render('apply', _.assign({}, strings.apply, user))
@@ -45,46 +46,56 @@ router.post('/', validate, rateLimit(), (req, res) => {
       return res.redirect('/')
     }
 
-    debug('Received application from "%s <%s>"', user.displayName, user.email)
+    debug('Received application from "%s <%s>"', user.name, user.email, user)
 
     res.redirect('/thanks')
+
+    const fields = _.map(
+      // omit comments because it's used for "text" above
+      _.toPairs(_.assign(
+        {},
+        _.omit(req.body, 'comments'),
+        // remove extraneous and falsey properties from user
+        _.omit(
+          _.omitBy(user._doc, x => _.isNil(x) || _.isEqual(false, x)),
+          ['__v', '_id', 'githubId', 'avatar_url', 'html_url', 'created_at', 'updated_at']
+        )
+      )),
+      // transform the field data
+      _.flow(
+        // necessary for unknown reasons
+        x => x,
+        // convert field names to title case
+        _.partialRight(_.map, (str, i) => {
+          return i ? str : changeCase.title(str)
+        }),
+        // convert array values (e.g. checkbox fields) into a string
+        x => {
+          if (_.isArray(x[1])) {
+            x[1] = x[1].join(', ')
+          }
+          return x
+        },
+        // combine title and value into an object
+        _.partial(_.zipObject, ['title', 'value']),
+        // add "short" property to object
+        _.partialRight(_.assign, { short: true })
+      )
+    )
 
     slack({
       channel: channel,
       username: botName,
       icon_url: req.originUri + 'images/bot.png',
       attachments: [{
-        fallback: user.displayName + ' wants to join Slack',
-        author_name: user.displayName,
-        author_link: user.url,
-        author_icon: dotty.get(user, 'image.url'),
+        fallback: user.name + ' wants to join Slack',
+        author_name: user.name,
+        author_link: user.html_url,
+        author_icon: user.avatar_url,
         color: '#28f428',
         pretext: 'New invite request:',
         text: req.body.comments ? `${req.body.comments}` : undefined,
-        fields: _.map(
-          // omit comments because it's used for "text" above
-          _.pairs(_.omit(req.body, 'comments')),
-          // transform the field data
-          _.flow(
-            // necessary for unknown reasons
-            x => x,
-            // convert field names to title case
-            _.partialRight(_.map, (str, i) => {
-              return i ? str : changeCase.title(str)
-            }),
-            // convert array values (e.g. checbox fields) into a string
-            x => {
-              if (_.isArray(x[1])) {
-                x[1] = x[1].join(', ')
-              }
-              return x
-            },
-            // combine title and value into an object
-            _.partial(_.zipObject, ['title', 'value']),
-            // add "short" property to object
-            _.partialRight(_.assign, { short: true })
-          )
-        )
+        fields
       }]
     })
   })
